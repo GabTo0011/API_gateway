c51bf5a8b2989661ff11022345819d38eb3f3378
Gabriel T <gab.toledo@duocuc.cl>
2025-07-03 19:02:41 -0400

    gateway - agrega redirección a microservicio de usuarios con validación de rol para PUT y DELETE

---
+++ api-gateway/src/main/java/com/perfulandia/gateway/redirection/gestionusuario/GestionUsuarioPublicRoutes.java
@@ -0,0 +1,9 @@
+package com.perfulandia.gateway.redirection.gestionusuario;
+
+public class GestionUsuarioPublicRoutes {
+
+    public static final String[] GESTION_USUARIO_PUBLIC_GET = {
+        "/api/proxy/usuarios",
+        "/api/proxy/usuarios/"
+    };
+}
