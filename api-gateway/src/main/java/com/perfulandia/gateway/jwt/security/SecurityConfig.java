fca7b9ee6c7c3426d69d451c35950c4f26f7b564
Gabriel T <gab.toledo@duocuc.cl>
2025-07-03 20:14:16 -0400

    security - implementa seguridad JWT en el API Gateway con filtro, rutas públicas y validación de token

---
+++ api-gateway/src/main/java/com/perfulandia/gateway/jwt/security/SecurityConfig.java
@@ -0,0 +1,66 @@
+package com.perfulandia.gateway.jwt.security;
+
+import org.springframework.http.HttpMethod; // Asegúrate de importar esto arriba
+
+// rutas publicas
+import static com.perfulandia.gateway.jwt.security.PublicRoutes.*;                      //importa las rutas publicas de jwt
+import static com.perfulandia.gateway.redirection.cliente.ClientesPublicRoutes.*;       //importa las rutas publicas de API Clientes
+import static com.perfulandia.gateway.redirection.gestionusuario.GestionUsuarioPublicRoutes.*;  //importa las rutas publicas de API Gateway
+import static com.perfulandia.gateway.redirection.producto.ProductosPublicRoutes.*;     //importa las rutas publicas de API Productos
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final JwtFilter jwtFilter;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+
+                // URL públicas JWT
+                .requestMatchers(HttpMethod.POST, PUBLIC_POST).permitAll() // rutas publicas POST de PublicRoutes de JWT
+                .requestMatchers(HttpMethod.GET, PUBLIC_GET).permitAll() // rutas publicas GET de PublicRoutes de JWT
+
+                // URL públicas API Gestion Usuario
+                .requestMatchers(HttpMethod.GET, GESTION_USUARIO_PUBLIC_GET).permitAll()   // lista pública api GESTION GET
+
+                // URL públicas API Productos
+                .requestMatchers(HttpMethod.GET, PRODUCTOS_PUBLIC_GET).permitAll()   // lista pública api Productos GET
+
+                // URL públicas API Clientes
+                .requestMatchers(HttpMethod.GET, CLIENTES_PUBLIC_GET).permitAll()   // lista pública api Clientes GET
+                
+                // Otras URL Token obligatorio
+                .anyRequest().authenticated()
+
+            )
+            .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+            .build();
+    }
+
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config)
+            throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
