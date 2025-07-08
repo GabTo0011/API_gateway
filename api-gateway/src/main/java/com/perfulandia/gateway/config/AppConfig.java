592c749e4290df8836848aa37ecb19f2bd971868
Gabriel T <gab.toledo@duocuc.cl>
2025-07-08 16:27:07 -0400

    chore(config): añade comentarios JavaDoc a clase AppConfig y a método RestTemplate

--- api-gateway/src/main/java/com/perfulandia/gateway/config/AppConfig.java
+++ api-gateway/src/main/java/com/perfulandia/gateway/config/AppConfig.java
@@ -4,11 +4,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+/**
+ * Clase configuracion de Spring para definir y personalizar Beans 
+**/
+
+
 @Configuration
 public class AppConfig {
     
-    // configuracion para declarar el consumo de otros
-    // microservicios.
+    // configuracion que declara el consumo de otros
+    // microservicios externos.
     @Bean
     public RestTemplate restTemplate(){
         return new RestTemplate();
